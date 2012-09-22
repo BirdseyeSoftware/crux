bec3e9860b2590c3b8dd4d24d9f66c734a8da09d
Tavis Rudd <dev+tavis@100daysofcode.com>
2012-09-22 13:15:01 -0700

    whitespace cleanup and converted properties to map

--- test/crux/example/tickets.clj
+++ test/crux/example/tickets.clj
@@ -26,68 +26,68 @@
 ;; optional vs required params
 
 (defn build-test-domain-spec []
- (create-domain {:name "tickets-domain"}
-  (field-types {user-id :User
-                name :String})
+  (create-domain {:name "tickets-domain"}
+    (field-types {user-id :User
+                  name :String})
 
 
-  (entity User [first last email]
-          (id-field oid :Guid)
-    (plural Users)
-    (events
-     [Created Create [first last email locked]
-      (merge event)]
-     [Locked Lock [why]
-      (assoc :locked true)]))
+    (entity User [first last email]
+            (id-field oid :Guid)
+      (plural Users)
+      (events
+        [Created Create [first last email locked]
+         (merge event)]
+        [Locked Lock [why]
+         (assoc :locked true)]))
 
-  (entity Ticket ~ticket-fields
-          (plural Tickets)
-    (type-infer-rule #"\?$" :Bool)
-    (field-types ~ticket-field-types)
-    (field-types {Assigned/assignee :User})
-    #_(conflict-detector )
-    (properties
-     last-reader (last (:read-by entity))
-     open? (= :open (:state entity))
-     ok?  (= "ok" (:title entity)))
+    (entity Ticket ~ticket-fields
+            (plural Tickets)
+      (type-infer-rule #"\?$" :Bool)
+      (field-types ~ticket-field-types)
+      (field-types {Assigned/assignee :User})
+      #_(conflict-detector )
+      (properties
+       {last-reader (last (:read-by entity))
+        open? (= :open (:state entity))
+        ok?  (= "ok" (:title entity))})
 
-    (events
-     [Created Create ~ticket-fields
-      (merge {:read-count 0}
-             (filter second event))]
+      (events
+        [Created Create ~ticket-fields
+         (merge {:read-count 0}
+                (filter second event))]
 
-     [DetailsChanged Change ~ticket-fields
-      (merge (filter second event))]
+        [DetailsChanged Change ~ticket-fields
+         (merge (filter second event))]
 
-     [Assigned Assign [assignee]
-      {:entity-constraints [ok?]
-       :conflict-detector nil
-       :validations [(check
-                      (open? entity)
-                      ;(prop (get-entity 'User assignee) :active?)
-                      "Assignee must be active user")]}
-      (merge {:ok (ok? entity)} event)]
+        [Assigned Assign [assignee]
+         {:entity-constraints [ok?]
+          :conflict-detector nil
+          :validations [(check
+                         (open? entity)
+                                        ;(prop (get-entity 'User assignee) :active?)
+                         "Assignee must be active user")]}
+         (merge {:ok (ok? entity)} event)]
 
-     [Read Read [user-id]
-      (update-in [:read-by] conj user-id)
-      (update-in [:read-count] inc)]
+        [Read Read [user-id]
+         (update-in [:read-by] conj user-id)
+         (update-in [:read-count] inc)]
 
-     [Closed Close []
-      (assoc :state :closed)
-      (assoc :assignee nil)]
+        [Closed Close []
+         (assoc :state :closed)
+         (assoc :assignee nil)]
 
-     [CommentAdded Comment [foo bar]
-      (update-in [:comments] conj event)]
+        [CommentAdded Comment [foo bar]
+         (update-in [:comments] conj event)]
 
-     [AttachmentAdded Attach  [url private?]
-      (update-in [:attachments] merge {event-id event})]
+        [AttachmentAdded Attach  [url private?]
+         (update-in [:attachments] merge {event-id event})]
 
 
-     [AttachmentDeleted DeleteAttachment
-      [attachment-id]
-      (update-in [:attachments] dissoc event-id)])
-    (properties
-     ok? (= (:title entity) "not-ok")))))
+        [AttachmentDeleted DeleteAttachment
+         [attachment-id]
+         (update-in [:attachments] dissoc event-id)])
+      (properties
+       {ok? (= (:title entity) "not-ok")}))))
 
 
 (defn build-reified-test-domain-spec []
