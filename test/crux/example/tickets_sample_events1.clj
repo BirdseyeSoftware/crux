c28f3045c15cee7db377ddf30482323cabb0f401
Tavis Rudd <dev+tavis@100daysofcode.com>
2012-09-22 13:14:37 -0700

    added additional assignment to test data

--- test/crux/example/tickets_sample_events1.clj
+++ test/crux/example/tickets_sample_events1.clj
@@ -3,6 +3,7 @@
  :events [
           #tickets/TicketCreated {:title "Ticket 1 Foo"}
           #tickets/TicketDetailsChanged {:title "Ticket 1 ok"}
+          #tickets/TicketAssigned {:assignee 987}
           #tickets/TicketAssigned {:assignee 123}
           #tickets/TicketClosed {}
           ]
