49d37c663a2471205a7e1269ef64ca16767b315d
Tavis Rudd <dev+tavis@100daysofcode.com>
2012-09-22 13:14:06 -0700

    initial import

---
+++ test/crux/example/ticket_sample_events2.clj
@@ -0,0 +1,5 @@
+#tickets/TicketCreated {:title "Ticket 1 Foo"}
+#tickets/TicketDetailsChanged {:title "Ticket 1 ok"}
+#tickets/TicketAssigned {:assignee 987}
+#tickets/TicketAssigned {:assignee 123}
+#tickets/TicketClosed {}
