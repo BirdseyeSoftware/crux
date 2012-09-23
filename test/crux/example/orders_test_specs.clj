d862c6561409e1168bbd99edd72022aae904173c
Tavis Rudd <tavis@damnsimple.com>
2012-09-22 20:46:29 -0700

    more events in test

--- test/crux/example/orders_test_specs.clj
+++ test/crux/example/orders_test_specs.clj
@@ -1,11 +1,11 @@
 {:entity Customer
  :name "Customer test 1"
- :events [
-          #orders/CustomerCreated {:name "Customer 1"}
-          #orders/CustomerOrderPlaced {:product :coffee
-                                       :unit-price 100
-                                       :qty 2}
-          #orders/CustomerOrderPlaced {:product :a-book
-                                       :unit-price 50
-                                       :qty 1}]
- :expected {:name "Customer 1" :outstanding-balance 250}}
+ :events
+ [
+  #orders/CustomerCreated {:name "Customer 1"}
+  #orders/CustomerOrderPlaced {:product :coffee :unit-price 100 :quantity 2}
+  #orders/CustomerOrderPlaced {:product :book1 :unit-price 50 :quantity 1}
+  #orders/CustomerBalancePaid {:amount 90}
+  #orders/CustomerOrderPlaced {:product :book2 :unit-price 15 :quantity 2}
+  ]
+ :expected {:name "Customer 1" :outstanding-balance 190}}
