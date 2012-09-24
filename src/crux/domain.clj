ed755b08999c176bf312666764be46855c7a3841
Tavis Rudd <tavis@damnsimple.com>
2012-09-24 08:03:33 -0700

    move command constraint / validation checking to separate ns

--- src/crux/domain.clj
+++ src/crux/domain.clj
@@ -440,25 +440,3 @@
                                 :type (or id-type :guid)}))
 (defmacro id-field [entity-spec id-field-name & [id-type]]
   `(id-field* ~entity-spec '~id-field-name ~id-type))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Command constraint and validation checking
-
-(defn first-unmet-constraint [entity constraint-forms+preds]
-  (some (fn -or* [[form pred]] (if-not (pred entity) form))
-        constraint-forms+preds))
-
-(defn unmet-constraints [entity constraint-forms+preds]
-  (for [[form pred] constraint-forms+preds
-        :when (not (pred entity))]
-    form))
-
-(defn unmet-validations [validation-args validation-forms+vfns]
-  (for [[form vfn] validation-forms+vfns
-        [error-message] [[(vfn validation-args)]]
-        :when (not (nil? error-message))]
-    {:error-message error-message
-     :validation-form form}))
-
-(defn first-unmet-validation [validation-args validation-forms+vfns]
-  (first (unmet-validations validation-args validation-forms+vfns)))
