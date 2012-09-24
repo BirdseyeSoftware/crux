61f15720195aeaf065e108396732a54b8e1146fe
Tavis Rudd <tavis@damnsimple.com>
2012-09-24 08:05:47 -0700

    extend order test spec

--- test/crux/example/orders_test_specs.clj
+++ test/crux/example/orders_test_specs.clj
@@ -1,5 +1,5 @@
 {:entity Customer
- :name "Customer test 1"
+ :name "Simple test of :outstanding-balance state machine"
  :events
  [
   #orders/CustomerCreated {:name "Customer 1"}
@@ -7,5 +7,7 @@
   #orders/CustomerOrderPlaced {:product :book1 :unit-price 50 :quantity 1}
   #orders/CustomerBalancePaid {:amount 90}
   #orders/CustomerOrderPlaced {:product :book2 :unit-price 15 :quantity 2}
+  #orders/CustomerBalancePaid {:amount 2}
+  #orders/CustomerOrderPlaced {:product :book2 :unit-price 15 :quantity 2}
   ]
- :expected {:name "Customer 1" :outstanding-balance 190}}
+ :expected {:name "Customer 1" :outstanding-balance 218}}
