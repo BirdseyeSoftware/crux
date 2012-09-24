7ac6e472d3e681544d4274e5fa40bd813ae47b22
Tavis Rudd <tavis@damnsimple.com>
2012-09-24 08:05:30 -0700

    revert to just defdomain rather than manually calling create-domain
    This makes the test much faster and the example easier to read.

--- test/crux/example/tickets.clj
+++ test/crux/example/tickets.clj
@@ -25,73 +25,68 @@
 (def ticket-fields (into [] (keys ticket-field-types)))
 ;; optional vs required params
 
+(defdomain tickets
+  (entity User [first last email locked]
+    (plural Users)
+    (events
+      [Created Create [first last email locked]
+       (merge event)]
+      [Locked Lock [why]
+       (assoc :locked true)]))
+
+  (entity Ticket ~ticket-fields
+          (plural Tickets)
+    (type-infer-rule #"\?$" :Bool)
+    (field-types ~ticket-field-types)
+    (field-types {Assigned/assignee :User})
+
+    (properties
+     {last-reader (last (:read-by entity))
+      open? (= :open (:state entity))
+      ok?  (= "ok" (:title entity))})
+
+    (events
+      [Created Create entity-fields
+       (merge {:read-count 0
+               :read-by []}
+              (filter second event))]
+
+      [DetailsChanged Change entity-fields
+       (merge (filter second event))]
+
+      [Assigned Assign [assignee]
+       {:entity-constraints [ok?]
+        :conflict-detector nil
+        :validations [(check
+                       (open? entity)
+                       "Assignee must be active user")]}
+       (assoc :assignee assignee)]
+
+      [Read Read [user-id]
+       (update-in [:read-by] conj user-id)
+       (update-in [:read-count] inc)]
+
+      [Closed Close []
+       (assoc :state :closed)
+       (assoc :assignee nil)]
+
+      [CommentAdded Comment [foo bar]
+       (update-in [:comments] conj event)]
+
+      [AttachmentAdded Attach  [url private?]
+       (update-in [:attachments] merge {event-id event})]
+
+      [AttachmentDeleted DeleteAttachment
+       [attachment-id]
+       (update-in [:attachments] dissoc event-id)])
+
+    (properties
+     {ok? (= (:title entity) "not-ok")})))
+
 (defn build-test-domain-spec []
-  (create-domain {:name "tickets-domain"}
-    (field-types {user-id :User
-                  name :String})
+  tickets)
 
-
-    (entity User [first last email]
-            (id-field oid :Guid)
-      (plural Users)
-      (events
-        [Created Create [first last email locked]
-         (merge event)]
-        [Locked Lock [why]
-         (assoc :locked true)]))
-
-    (entity Ticket ~ticket-fields
-            (plural Tickets)
-      (type-infer-rule #"\?$" :Bool)
-      (field-types ~ticket-field-types)
-      (field-types {Assigned/assignee :User})
-      #_(conflict-detector )
-      (properties
-       {last-reader (last (:read-by entity))
-        open? (= :open (:state entity))
-        ok?  (= "ok" (:title entity))})
-
-      (events
-        [Created Create ~ticket-fields
-         (merge {:read-count 0}
-                (filter second event))]
-
-        [DetailsChanged Change ~ticket-fields
-         (merge (filter second event))]
-
-        [Assigned Assign [assignee]
-         {:entity-constraints [ok?]
-          :conflict-detector nil
-          :validations [(check
-                         (open? entity)
-                                        ;(prop (get-entity 'User assignee) :active?)
-                         "Assignee must be active user")]}
-         (merge {:ok (ok? entity)} event)]
-
-        [Read Read [user-id]
-         (update-in [:read-by] conj user-id)
-         (update-in [:read-count] inc)]
-
-        [Closed Close []
-         (assoc :state :closed)
-         (assoc :assignee nil)]
-
-        [CommentAdded Comment [foo bar]
-         (update-in [:comments] conj event)]
-
-        [AttachmentAdded Attach  [url private?]
-         (update-in [:attachments] merge {event-id event})]
-
-
-        [AttachmentDeleted DeleteAttachment
-         [attachment-id]
-         (update-in [:attachments] dissoc event-id)])
-      (properties
-       {ok? (= (:title entity) "not-ok")}))))
-
+(def reified-tickets  (reify-domain-spec! tickets))
 
 (defn build-reified-test-domain-spec []
-  (-> (build-test-domain-spec)
-      reify-domain-spec!))
-
-;(build-test-domain-spec)
+  reified-tickets)
