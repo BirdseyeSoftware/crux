41625bb959231513dd9801f6a6040607884ec73b
Roman Gonzalez <romanandreg@gmail.com>
2012-09-25 16:22:18 -0700

    Doing small kaizen to crux.domain and crux.util
    * Moving some functions from domain that belongs to util
    * Adding new functions to the ICruxSpec (not being used yet, but
      hopefully will be used when code is more consistent)
    * Inlining function `validate-entity-spec`, tried to do the
      same with `validate-domain-spec` but function is not found, some
      weird bug on clojure perhaps?
    * Adding type-symbol function to get the symbol of an instance type
    * Adding new keys to the crux.internal.keys namespace

--- src/crux/internal/keys.clj
+++ src/crux/internal/keys.clj
@@ -16,16 +16,25 @@
 (defonce CONSTRAINTS :entity-constraints)
 (defonce VALIDATIONS :validations)
 (defonce FIELDS :fields)
+
 (defonce ENTITIES :entities)
 (defonce ENTITY-SYMBOL :entity-symbol)
+(defonce ENTITY-NAME :entity-name)
+
 (defonce EVENTS :events)
+(defonce EVENT-NAME :event-name)
 (defonce EVENT-SYMBOL :event-symbol)
 (defonce FULL-EVENT-SYMBOL :full-event-symbol)
+
+(defonce COMMAND-NAME :event-name)
 (defonce COMMAND-SYMBOL :command-symbol)
 (defonce FULL-COMMAND-SYMBOL :full-command-symbol)
+
 (defonce DEFAULTS :defaults)
+
 (defonce PROPERTIES :properties)
 (defonce PROPERTY-FORMS :property-forms)
 (defonce COMMAND-CONSTRAINTS :command-constraints)
 (defonce COMMAND-VALIDATORS :command-validators)
+
 (defonce ID-FIELD :id-field)
